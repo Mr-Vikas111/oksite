1f989424d1f71c82935c8266895a1e2663931583
vikasminj <vikashminj1999@gmail.com>
2021-04-25 12:14:14 +0530

    update1.0

---
+++ JokeAPI/script.js
@@ -0,0 +1,26 @@
+//promises
+//GET https://icanhazdadjoke.com/
+
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokebtn');
+
+const generateJokes = () => {
+
+    const setHeader = {
+        headers: {
+            Accept: "application/json"
+
+        }
+
+    }
+
+    fetch('https://icanhazdadjoke.com', setHeader)
+        .then((res) => res.json())
+        .then((data) => {
+            jokes.innerHTML = `" ${data.joke} "`;
+        }).catch((error) => {
+            console.log(error);
+        })
+}
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
